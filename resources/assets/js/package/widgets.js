bdc9d4d24458c1320d91f6610f9914e7d6c0e6d3
Konstantin Bukin <bukin@inetstudio.ru>
2018-05-26 23:27:23 +0300

    Add js module methods

--- resources/assets/js/package/widgets.js
+++ resources/assets/js/package/widgets.js
@@ -1,7 +1,8 @@
 window.Admin = window.Admin || {
     options: {
         tinymce: {}
-    }
+    },
+    modules: {}
 };
 
 $.extend(window.Admin.options.tinymce, {
@@ -15,5 +16,46 @@ $.extend(window.Admin.options.tinymce, {
                 }).get()
             );
         });
+    }
+});
+
+$.extend(window.Admin.modules, {
+    widgets: {
+        getWidget: function (widgetID, callback) {
+            $.ajax({
+                url: route('back.widgets.show', widgetID),
+                method: 'GET',
+                dataType: 'json',
+                success: function (widget) {
+                    callback(widget);
+                },
+                error: function () {
+                    swal({
+                        title: "Ошибка",
+                        text: "Произошла ошибка при получении виджета",
+                        type: "error"
+                    });
+                }
+            });
+        },
+        saveWidget: function (widgetID, widgetData, widgetOptions) {
+            let url = (widgetID !== '') ? route('back.widgets.update', widgetID): route('back.widgets.store');
+
+            if (widgetID !== '') {
+                $.extend(widgetData, {
+                    _method: 'PUT'
+                })
+            }
+
+            $.ajax({
+                url: url,
+                method: 'POST',
+                data: widgetData,
+                dataType: 'json',
+                success: function (widget) {
+                    widgetOptions.editor.execCommand('mceReplaceContent', false, '<img class="content-widget" data-type="'+widgetOptions.type+'" data-id="'+widget.id+'" alt="'+widgetOptions.alt+'" style="height: 100px; width: 100%; border: 1px red solid;" />');
+                }
+            });
+        }
     }
 });
