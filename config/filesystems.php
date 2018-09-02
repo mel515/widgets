8190ee7a2abfea8ceb283c269bd07ad168cba06d
Konstantin Bukin <bukin@inetstudio.ru>
2018-09-03 01:08:20 +0300

    Fix storage urls

--- config/filesystems.php
+++ config/filesystems.php
@@ -9,8 +9,8 @@ return [
 
     'widgets' => [
         'driver' => 'local',
-        'root' => storage_path('app/public/widgets/'),
-        'url' => env('APP_URL').'/storage/widgets/',
+        'root' => storage_path('app/public/widgets'),
+        'url' => env('APP_URL').'/storage/widgets',
         'visibility' => 'public',
     ],
 
